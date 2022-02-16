ef726dbb597fecc466147dc2d53085434133538e
GeorgeM101 <georgmboya@gmail.com>
2022-02-16 07:48:36 +0300

    added index page

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,TextAreaField,BooleanField
-from wtforms.validators import InputRequired,Email,EqualTo
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Email, EqualTo
 from app.models import user
 from wtforms import ValidationError
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Enter contact name', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Add contact')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,21 +1,48 @@
 from click import confirm
-from flask import render_template,redirect,url_for,flash,request
+from flask import render_template, redirect, url_for, flash, request
 from . import auth
+from ..models import user
+from .forms import RegistrationForm, LoginForm
+from .. import db
+from flask_login import current_user, login_user, logout_user, login_required
 
-from flask_login import login_user,logout_user,login_required
+from app import app
 '''
 add imports from forms here
 '''
 from ..models import user
 from .. import db
 
-user=user()
+user = user()
 
-@auth.route('/login',methods=['GET','POST'])
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template('index.html')
+
+
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = user(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', registration_form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-  '''
-  add login form route
-  '''
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = user.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
+        flash('Invalid username or Password')
 
-
+    title = "Pitch-App Login"
+    return render_template('auth/login.html', login_form=login_form, title=title)
